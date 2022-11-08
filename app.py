0e454f472e27dbfca40e3c449989fc7cc53cf23c
yaoj90 <sherryyaoj@hotmail.com>
2022-11-08 23:12:23 +0100

    commit

--- app.py
+++ app.py
@@ -49,5 +49,9 @@ with st.container():
 # ---- CONTACT ----
 with st.container():
     st.write("---")
+    st.header("Get In Touch With Me!")
+    st.write("##")
+
     st.header('<a href="yao.jiang@ntnu.no">Contact me !</a>', unsafe_allow_html=True)
-    
+
+
