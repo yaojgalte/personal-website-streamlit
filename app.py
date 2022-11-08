1d79edb3762997933311d4867b356e2d465426e3
yaoj90 <sherryyaoj@hotmail.com>
2022-11-08 22:51:58 +0100

    commit

--- app.py
+++ app.py
@@ -30,34 +30,13 @@ img_diabetes = Image.open("images/f_doctor.png")
 
 # ---- HEADER SECTION ----
 with st.container():
-    st.subheader("Hi, I am Sven :wave:")
-    st.title("A Data Analyst From Germany")
+    st.subheader("Hi, I am Yao :wave:")
+    st.title("A Postdoc From Norway")
     st.write(
-        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+        "I like programming and solving technical problems, my goal for the future is to become a proficient programmer."
+        " I like to write neat and efficient code, and to design and develop tools for meaningful applications."
     )
-    st.write("[Learn More >](https://pythonandvba.com)")
-
-# ---- WHAT I DO ----
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header("What I do")
-        st.write("##")
-        st.write(
-            """
-            On my YouTube channel I am creating tutorials for people who:
-            - are looking for a way to leverage the power of Python in their day-to-day work.
-            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-            - are working with Excel and found themselves thinking - "there has to be a better way."
-
-            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
-            """
-        )
-        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
-    with right_column:
-        st_lottie(lottie_coding, height=300, key="coding")
+    st.write("[Learn More >](https://github.com/yaoj90?tab=repositories)")
 
 # ---- PROJECTS ----
 with st.container():
