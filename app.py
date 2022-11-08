ec3367940920e0c0fbe0cc8e8f68f74230c98fec
yaoj90 <sherryyaoj@hotmail.com>
2022-11-08 23:06:57 +0100

    commit

--- app.py
+++ app.py
@@ -40,11 +40,8 @@ with st.container():
     st.write("---")
     st.header("My Web Apps")
     st.write("##")
-    image_column, text_column = st.columns((1, 2))
-    
-    with text_column:
-        st.subheader("Diabetes Predictor")
-        st.markdown("[Let's Play!](https://diabetes-predictor.streamlit.app)")
+    st.subheader("Diabetes Predictor")
+    st.markdown("[Let's Play!](https://diabetes-predictor.streamlit.app)")
 
 # ---- CONTACT ----
 with st.container():
