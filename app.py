5992a89f3cfeff89cf2bb3d6ef58840c6aca7726
yaoj90 <sherryyaoj@hotmail.com>
2022-11-08 23:11:36 +0100

    commit

--- app.py
+++ app.py
@@ -52,20 +52,6 @@ with st.container():
     st.header("Get In Touch With Me!")
     st.write("##")
 
-    st.sidebar.markdown('<a href="mailto:hello@streamlit.io">Contact us !</a>', unsafe_allow_html=True)
+    st.markdown('<a href="yao.jiang@ntnu.no">Contact me !</a>', unsafe_allow_html=True)
 
-    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-    contact_form = """
-    <form action="https://formsubmit.co/yao.jiang@ntnu.no" method="POST">
-        <input type="hidden" name="_captcha" value="false">
-        <input type="text" name="name" placeholder="Your name" required>
-        <input type="email" name="email" placeholder="Your email" required>
-        <textarea name="message" placeholder="Your message here" required></textarea>
-        <button type="submit">Send</button>
-    </form>
-    """
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.markdown(contact_form, unsafe_allow_html=True)
-    with right_column:
-        st.empty()
+    
